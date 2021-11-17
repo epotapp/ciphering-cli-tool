8cc4fced0edde490519f32e334e81643267c9a99
epotapp <93080355+epotapp@users.noreply.github.com>
2021-11-17 19:56:22 +0300

    Fixed exports
    Export of constant contained transform stream didn't work and was removed.
    Currently uses module.exports of inherited class from transform stream.
    Basic functionality completed.

---
+++ transrot8.js
@@ -0,0 +1,40 @@
+const { Transform } = require("stream");
+
+function rot8Func(str) {
+  let array = str.split("");
+  let transformedArray = [];
+
+  for (i = 0; i < array.length; i++) {
+    let inpLetterIndex = array[i].charCodeAt();
+
+    if (
+      (inpLetterIndex > 64 && inpLetterIndex < 83) ||
+      (inpLetterIndex > 96 && inpLetterIndex < 115)
+    ) {
+      transformedArray.push(String.fromCharCode(inpLetterIndex + 8));
+    } else if (inpLetterIndex > 82 && inpLetterIndex < 91) {
+      transformedArray.push(String.fromCharCode(inpLetterIndex - 18));
+    } else if (inpLetterIndex > 114 && inpLetterIndex < 123) {
+      transformedArray.push(String.fromCharCode(inpLetterIndex - 18));
+    } else transformedArray.push(array[i]);
+  }
+
+  return transformedArray.join("");
+}
+
+class TransRot8 extends Transform {
+  constructor() {
+    super();
+  }
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
+
+    const transformedChunk = rot8Func(chunkStringified);
+    this.push(transformedChunk);
+    callback();
+  }
+}
+
+//const rot8 = new TransRot8();
+module.exports = TransRot8;
+//exports.rot8 = new TransRot8();
