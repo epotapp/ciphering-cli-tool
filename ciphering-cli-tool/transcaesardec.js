0df638e4ad5f72f148eb352278a4a8dad7f2f999
epotapp <93080355+epotapp@users.noreply.github.com>
2021-11-17 20:26:35 +0300

    Fixed exports and functionality finalized
    Link: Task 1
    Date of deadline: 14.11.2021
    Date of completion: 17.11.2021
    
    Exported constant didn't work and was removed by module.exports of class inherited from transform stream.
    Basic functionality was completed.
    
    Score is 93 with penalties.

--- ciphering-cli-tool/transcaesardec.js
+++ ciphering-cli-tool/transcaesardec.js
@@ -1,50 +1,43 @@
-const { Transform } = require('stream');
+const { Transform } = require("stream");
+const TransCaesar = require("./transcaesar");
 
-function caesarDecFunc (str) {
-        
-    let array = str.split('');
-    let transformedArray = [];
-    
-    for (i = 0; i < array.length; i++) {
-        
-        let inpLetterIndex = array[i].charCodeAt();
-        
-        if ( ( (inpLetterIndex > 65) && (inpLetterIndex < 91 ) ) || ( (inpLetterIndex > 97) && (inpLetterIndex < 123) ) ) {
-            transformedArray.push(String.fromCharCode(inpLetterIndex - 1));
-            
-        } else if (inpLetterIndex == 65) {
-            transformedArray.push('Z');                
-            
-        } else if (inpLetterIndex == 97) {
-            transformedArray.push('z');                
-            
-        } else transformedArray.push(array[i]);
-    }   
-    
-    return transformedArray.join('');
-    
+function caesarDecFunc(str) {
+  let array = str.split("");
+  let transformedArray = [];
+
+  for (i = 0; i < array.length; i++) {
+    let inpLetterIndex = array[i].charCodeAt();
+
+    if (
+      (inpLetterIndex > 65 && inpLetterIndex < 91) ||
+      (inpLetterIndex > 97 && inpLetterIndex < 123)
+    ) {
+      transformedArray.push(String.fromCharCode(inpLetterIndex - 1));
+    } else if (inpLetterIndex == 65) {
+      transformedArray.push("Z");
+    } else if (inpLetterIndex == 97) {
+      transformedArray.push("z");
+    } else transformedArray.push(array[i]);
+  }
+
+  return transformedArray.join("");
 }
-
 
 class TransCaesarDec extends Transform {
-    
-    constructor ( options = {} ) {
-        super(options);
-    }
+  constructor() {
+    super();
+  }
 
-    _transform(chunk, encoding, callback) {
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
 
-        const chunkStringified = chunk.toString();
+    const transformedChunk = caesarDecFunc(chunkStringified);
+    this.push(transformedChunk);
 
-        const transformedChunk = caesarDecFunc(chunkStringified)
-
-        callback(null, transformedChunk);
-
-    } 
-
+    callback();
+  }
 }
 
-
-const caesardec = new TransCaesarDec();
-
-exports.caesardec = caesardec;
+//const caesardec = new TransCaesarDec();
+module.exports = TransCaesarDec;
+//exports.caesardec = new TransCaesarDec();
