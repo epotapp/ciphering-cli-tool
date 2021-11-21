b516a4b2fc2445afd04376b73b36a5de818fb6fb
epotapp <93080355+epotapp@users.noreply.github.com>
2021-11-21 19:20:55 +0300

    Add files via upload

--- src/configDecoder.js
+++ src/configDecoder.js
@@ -1,3 +1,4 @@
+const {stdout} = require('process');
 const caesar = require("./streams/transcaesar");
 const caesardec = require("./streams/transcaesardec");
 const atbash = require("./streams/transatbash");
@@ -6,47 +7,38 @@ const rot8dec = require("./streams/transrot8dec");
 
 const configValidator = require('./validatorconfig');
 
-
-var configArray = configValidator(process.argv);
-
-
-console.log('');
-console.log('Начало работы программы.');
-console.log('');
-console.log('Используется следующая конфигурация шифров:');
+const configArray = configValidator(process.argv);
 
 
-var streamArray = [];
+module.exports = function configDecoder() {
 
-for (let code = 0; code < configArray.length; code++) {
-  console.log(configArray[code]);
+    let streamArray = [];
+  
+    for (let code = 0; code < configArray.length; code++) {
 
-  switch (configArray[code]) {
-    case "C0":
-      streamArray.push(new caesardec);
-      console.log("Дешифровка Цезаря");
-      break;
-    case "C1":
-      streamArray.push(new caesar);
-      console.log("Шифр Цезаря");
-      break;
-    case "A":
-      streamArray.push(new atbash);
-      console.log("Шифр Атбаш");
-      break;
-    case "R0":
-      streamArray.push(new rot8dec);
-      console.log("Дешифровка Rot8");
-      break;
-    case "R1":
-      streamArray.push(new rot8);
+        switch (configArray[code]) {
+            case "C0":
+              streamArray.push(new caesardec);
+              break;
+            case "C1":
+              streamArray.push(new caesar);
+              break;
+            case "A":
+              streamArray.push(new atbash);
+              break;
+            case "R0":
+              streamArray.push(new rot8dec);
+              break;
+            case "R1":
+              streamArray.push(new rot8);
+              break;
+            default:
+              throw validationError = new Error(`Ошибка валидации конфига`);
+        }
+    }
 
-      console.log("Шифр Rot8");
-      break;
-    default:
-      throw validationError = new Error(`Ошибка валидации конфига`);
-  }
+
+    return streamArray;
+
 }
 
-
-module.exports = { streamArray };
